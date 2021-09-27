e4bb33ed8c05c7916fe77c604182e8caceba7394
mwhy25 <mwhy25@gmail.com>
2021-09-27 13:54:10 +0700

    main

--- src/App.js
+++ src/App.js
@@ -15,9 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
+            <Profile></Profile>
     </div>
   );
 }
