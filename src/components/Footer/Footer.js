84d0db3a6fe738fd84fbb085d5cd30ffcfad1746
unknown <mwhy25@gmail.com>
2021-10-12 21:40:31 +0700

    Update Header

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,24 @@
-import React from 'react'
-
+import { Typography } from "@material-ui/core";
+import React from "react";
+import "./Footer.css";
 const Footer = () => {
-    return (
-        <div>
-            This is footer      
-        </div>
-    )
-}
+  return (
+    <div className="footer">
+      <div className="footer_left">
+        <Typography className="footer_name"></Typography>
+      </div>
+      <div className="footer_right">
+        <Typography className="footer_copyright">
+          Design and Developed by{" "}
+          <a href="/" target="_blank">
+            Muhammad Wahyu
+          </a>
+          .
+          <br />
+        </Typography>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
