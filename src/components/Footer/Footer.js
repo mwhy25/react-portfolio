d426888b7fab4f50b56008389fd76ccce8868c82
unknown <mwhy25@gmail.com>
2021-10-13 20:42:03 +0700

    Finish Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,12 @@
 import { Typography } from "@material-ui/core";
 import React from "react";
 import "./Footer.css";
+import resumeData from "../../utils/resumeData"
 const Footer = () => {
   return (
     <div className="footer">
       <div className="footer_left">
-        <Typography className="footer_name"></Typography>
+        <Typography className="footer_name">{resumeData.name}</Typography>
       </div>
       <div className="footer_right">
         <Typography className="footer_copyright">
