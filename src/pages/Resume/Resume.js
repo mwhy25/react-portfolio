0a367ef7b22ff5b85187feb084b2facfe1f25efb
unknown <mwhy25@gmail.com>
2021-10-13 22:22:21 +0700

    Finish About me

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,11 +1,34 @@
-import React from 'react'
-
+import { Container, Grid, Typography } from "@material-ui/core";
+import React from "react";
+import resumeData from "../../utils/resumeData";
+import "./Resume.css";
 const Resume = () => {
-    return (
-        <div>
-            This is Resume
-        </div>
-    )
-}
+  return (
+    <>
+      {/* About me */}
+      <Grid container className="section pb_45">
+        <Grid item className="section_title mb_30">
+          <span></span>
+          <h6 className="section_title_text">About Me</h6>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body2"className="aboutme_text">{resumeData.about}</Typography>
+        </Grid>
+      </Grid>
 
-export default Resume
+      {/* Education and Experiences */}
+      <Grid container className="section"></Grid>
+
+      {/* Services */}
+      <Grid container className="section"></Grid>
+
+      {/* Skills */}
+      <Grid container className="section"></Grid>
+
+      {/* Contact */}
+      <Grid container className="section"></Grid>
+    </>
+  );
+};
+
+export default Resume;
--- src/utils/resumeData.js
+++ src/utils/resumeData.js
@@ -34,6 +34,7 @@ const resumeData = {
         icon: <GitHubIcon />,
       },
     },
+    about:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et leo duis ut diam quam. Vitae suscipit tellus mauris a diam. Neque laoreet suspendisse interdum consectetur. Vitae justo eget magna fermentum iaculis eu. Et magnis dis parturient montes nascetur ridiculus mus mauris. A pellentesque sit amet porttitor eget dolor morbi non. Facilisis mauris sit amet massa vitae. Sed velit dignissim sodales ut eu. Bibendum est ultricies integer quis. Nulla pharetra diam sit amet nisl suscipit adipiscing. \n \n Consectetur a erat nam at. Vitae nunc sed velit dignissim sodales ut eu sem. Ut morbi tincidunt augue interdum velit euismod in pellentesque massa. Sit amet consectetur adipiscing elit duis tristique sollicitudin nibh. Cras adipiscing enim eu turpis egestas. Arcu odio ut sem nulla pharetra diam sit. Habitasse platea dictumst quisque sagittis purus. Tempor commodo ullamcorper a lacus vestibulum sed arcu non odio. In tellus integer feugiat scelerisque varius morbi enim nunc faucibus. Ultricies lacus sed turpis tincidunt id aliquet risus feugiat in. A iaculis at erat pellentesque adipiscing commodo elit at imperdiet. Morbi non arcu risus quis varius. At in tellus integer feugiat scelerisque varius morbi. Dui accumsan sit amet nulla. Eu tincidunt tortor aliquam nulla facilisi. Amet cursus sit amet dictum sit amet justo donec.",
   };
   
 
