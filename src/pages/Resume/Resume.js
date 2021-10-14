4c87e0e93f1de4b5cb16a4a0708f5ce4968116d0
unknown <mwhy25@gmail.com>
2021-10-15 06:30:41 +0700

    Progress Resume

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -2,6 +2,10 @@ import { Container, Grid, Typography } from "@material-ui/core";
 import React from "react";
 import resumeData from "../../utils/resumeData";
 import "./Resume.css";
+import CustomTimeline, {
+  CustomTimelineSeparator,
+} from "../../components/Timeline/CustomTimeline";
+import WorkIcon from '@mui/icons-material/Work';
 const Resume = () => {
   return (
     <>
@@ -12,12 +16,31 @@ const Resume = () => {
           <h6 className="section_title_text">About Me</h6>
         </Grid>
         <Grid item xs={12}>
-          <Typography variant="body2"className="aboutme_text">{resumeData.about}</Typography>
+          <Typography variant="body2" className="aboutme_text">
+            {resumeData.about}
+          </Typography>
         </Grid>
       </Grid>
 
       {/* Education and Experiences */}
-      <Grid container className="section"></Grid>
+      <Grid container className="section">
+        <Grid item className="section_title mb_30">
+          <span></span>
+          <h6 className="section_title_text">Resume</h6>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container>
+            {/* Experience */}
+            <Grid item sm={12} md={6}>
+              <CustomTimeline title="Work Experience" icon={<WorkIcon/>}>
+                {resumeData.experience.map('title')}
+              </CustomTimeline>
+            </Grid>
+            {/* Education */}
+            <Grid item sm={12} md={6}></Grid>
+          </Grid>
+        </Grid>
+      </Grid>
 
       {/* Services */}
       <Grid container className="section"></Grid>
--- src/utils/resumeData.js
+++ src/utils/resumeData.js
@@ -3,39 +3,73 @@ import FacebookIcon from "@mui/icons-material/Facebook";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 
-
 const resumeData = {
-    name: "Muhammad Wahyu",
-    title: "Web Developer",
-    birthday: "25th March 1995",
-    email: "mwhy25@gmail.com",
-    // address: "jalan sekian lama bertemu",
-    phone: "0897-8178-756",
-  
-    socials: {
-      Facebook: {
-        link: "https://www.facebook.com/mwhy25",
-        text: "M. Wahyu",
-        icon: <FacebookIcon />,
-      },
-      Twitter: {
-        link: "https://www.twitter.com/watop_",
-        text: "My Twitter",
-        icon: <TwitterIcon />,
-      },
-      LinkedIn: {
-        link: "https://www.linkedin.com/in/mwhy25",
-        text: "My LinkedIn",
-        icon: <LinkedInIcon />,
-      },
-      GitHub: {
-        link: "https://www.github.com/mwhy25",
-        text: "My GitHub",
-        icon: <GitHubIcon />,
-      },
-    },
-    about:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et leo duis ut diam quam. Vitae suscipit tellus mauris a diam. Neque laoreet suspendisse interdum consectetur. Vitae justo eget magna fermentum iaculis eu. Et magnis dis parturient montes nascetur ridiculus mus mauris. A pellentesque sit amet porttitor eget dolor morbi non. Facilisis mauris sit amet massa vitae. Sed velit dignissim sodales ut eu. Bibendum est ultricies integer quis. Nulla pharetra diam sit amet nisl suscipit adipiscing. \n \n Consectetur a erat nam at. Vitae nunc sed velit dignissim sodales ut eu sem. Ut morbi tincidunt augue interdum velit euismod in pellentesque massa. Sit amet consectetur adipiscing elit duis tristique sollicitudin nibh. Cras adipiscing enim eu turpis egestas. Arcu odio ut sem nulla pharetra diam sit. Habitasse platea dictumst quisque sagittis purus. Tempor commodo ullamcorper a lacus vestibulum sed arcu non odio. In tellus integer feugiat scelerisque varius morbi enim nunc faucibus. Ultricies lacus sed turpis tincidunt id aliquet risus feugiat in. A iaculis at erat pellentesque adipiscing commodo elit at imperdiet. Morbi non arcu risus quis varius. At in tellus integer feugiat scelerisque varius morbi. Dui accumsan sit amet nulla. Eu tincidunt tortor aliquam nulla facilisi. Amet cursus sit amet dictum sit amet justo donec.",
-  };
-  
+  name: "Muhammad Wahyu",
+  title: "Web Developer",
+  birthday: "25th March 1995",
+  email: "mwhy25@gmail.com",
+  // address: "jalan sekian lama bertemu",
+  phone: "0897-8178-756",
 
-  export default resumeData
+  socials: {
+    Facebook: {
+      link: "https://www.facebook.com/mwhy25",
+      text: "M. Wahyu",
+      icon: <FacebookIcon />,
+    },
+    Twitter: {
+      link: "https://www.twitter.com/watop_",
+      text: "My Twitter",
+      icon: <TwitterIcon />,
+    },
+    LinkedIn: {
+      link: "https://www.linkedin.com/in/mwhy25",
+      text: "My LinkedIn",
+      icon: <LinkedInIcon />,
+    },
+    GitHub: {
+      link: "https://www.github.com/mwhy25",
+      text: "My GitHub",
+      icon: <GitHubIcon />,
+    },
+  },
+  about:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et leo duis ut diam quam. Vitae suscipit tellus mauris a diam. Neque laoreet suspendisse interdum consectetur. Vitae justo eget magna fermentum iaculis eu. Et magnis dis parturient montes nascetur ridiculus mus mauris. A pellentesque sit amet porttitor eget dolor morbi non. Facilisis mauris sit amet massa vitae. Sed velit dignissim sodales ut eu. Bibendum est ultricies integer quis. Nulla pharetra diam sit amet nisl suscipit adipiscing. \n \n Consectetur a erat nam at. Vitae nunc sed velit dignissim sodales ut eu sem. Ut morbi tincidunt augue interdum velit euismod in pellentesque massa. Sit amet consectetur adipiscing elit duis tristique sollicitudin nibh. Cras adipiscing enim eu turpis egestas. Arcu odio ut sem nulla pharetra diam sit. Habitasse platea dictumst quisque sagittis purus. Tempor commodo ullamcorper a lacus vestibulum sed arcu non odio. In tellus integer feugiat scelerisque varius morbi enim nunc faucibus. Ultricies lacus sed turpis tincidunt id aliquet risus feugiat in. A iaculis at erat pellentesque adipiscing commodo elit at imperdiet. Morbi non arcu risus quis varius. At in tellus integer feugiat scelerisque varius morbi. Dui accumsan sit amet nulla. Eu tincidunt tortor aliquam nulla facilisi. Amet cursus sit amet dictum sit amet justo donec.",
+
+  experience: [
+    {
+      title: "Work 1",
+      date: "2017 - Present",
+      description: "Lorem imsum",
+    },
+    {
+      title: "Work 2",
+      date: "2017 - Present",
+      description: "Lorem imsum",
+    },
+    {
+      title: "Work 3",
+      date: "2017 - Present",
+      description: "Lorem imsum",
+    },
+  ],
+  education: [
+    {
+      title: "Education 1",
+      date: "2017 - Present",
+      description: "Lorem imsum",
+    },
+    {
+      title: "Education 2",
+      date: "2017 - Present",
+      description: "Lorem imsum",
+    },
+    {
+      title: "Education 3",
+      date: "2017 - Present",
+      description: "Lorem imsum",
+    },
+  ],
+};
+
+export default resumeData;
