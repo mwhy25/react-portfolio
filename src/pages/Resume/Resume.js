57f5715519b10cdbd2a09e9bf2870a3070bc3d31
unknown <mwhy25@gmail.com>
2021-10-17 21:56:14 +0700

    Update Resume - Finish Work & Education

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -5,7 +5,15 @@ import "./Resume.css";
 import CustomTimeline, {
   CustomTimelineSeparator,
 } from "../../components/Timeline/CustomTimeline";
-import WorkIcon from '@mui/icons-material/Work';
+import WorkIcon from "@mui/icons-material/Work";
+import SchoolIcon from "@mui/icons-material/School";
+import {
+  TimelineConnector,
+  TimelineContent,
+  TimelineDot,
+  TimelineItem,
+  TimelineSeparator,
+} from "@mui/lab";
 const Resume = () => {
   return (
     <>
@@ -29,15 +37,56 @@ const Resume = () => {
           <h6 className="section_title_text">Resume</h6>
         </Grid>
         <Grid item xs={12}>
-          <Grid container>
+          <Grid container className="resume_timeline">
             {/* Experience */}
             <Grid item sm={12} md={6}>
-              <CustomTimeline title="Work Experience" icon={<WorkIcon/>}>
-                {resumeData.experience.map('title')}
+              <CustomTimeline title="Work Experience" icon={<WorkIcon />}>
+                {resumeData.experience.map((experience) => (
+                  <TimelineItem>
+                    <CustomTimelineSeparator />
+                    <TimelineContent className="timeline_content">
+                      <Typography className="timeline_title">
+                        {experience.title}
+                      </Typography>
+                      <Typography variant="caption" className="timeline_date">
+                        {experience.date}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        className="timeline_description"
+                      >
+                        {experience.description}
+                      </Typography>
+                    </TimelineContent>
+                  </TimelineItem>
+                ))}
               </CustomTimeline>
             </Grid>
+
             {/* Education */}
-            <Grid item sm={12} md={6}></Grid>
+            <Grid item sm={12} md={6}>
+              <CustomTimeline title="Education" icon={<SchoolIcon />}>
+                {resumeData.education.map((education) => (
+                  <TimelineItem>
+                    <CustomTimelineSeparator />
+                    <TimelineContent className="timeline_content">
+                      <Typography className="timeline_title">
+                        {education.title}
+                      </Typography>
+                      <Typography variant="caption" className="timeline_date">
+                        {education.date}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        className="timeline_description"
+                      >
+                        {education.description}
+                      </Typography>
+                    </TimelineContent>
+                  </TimelineItem>
+                ))}
+              </CustomTimeline>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
--- src/utils/resumeData.js
+++ src/utils/resumeData.js
@@ -40,34 +40,34 @@ const resumeData = {
     {
       title: "Work 1",
       date: "2017 - Present",
-      description: "Lorem imsum",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
     {
       title: "Work 2",
       date: "2017 - Present",
-      description: "Lorem imsum",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
     {
       title: "Work 3",
       date: "2017 - Present",
-      description: "Lorem imsum",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
   ],
   education: [
     {
       title: "Education 1",
       date: "2017 - Present",
-      description: "Lorem imsum",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
     {
       title: "Education 2",
       date: "2017 - Present",
-      description: "Lorem imsum",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
     {
       title: "Education 3",
       date: "2017 - Present",
-      description: "Lorem imsum",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
   ],
 };
